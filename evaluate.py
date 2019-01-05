5d7bd724045d6a928a161f2fe2cc930bcb4646d7
daniel <daniel.koguciuk@gmail.com>
2019-01-05 14:50:06 +0000

    Added param num

--- evaluate.py
+++ evaluate.py
@@ -167,11 +167,12 @@ def eval_one_epoch(sess, ops, num_votes=1, topk=1):
     log_string('eval accuracy: %f' % (total_correct / float(total_seen)))
     log_string('eval avg class acc: %f' % (np.mean(np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float))))
     log_string('mean evaluation time of one batch: %f' % (total_time / total_batches))
-    
+    parameter_num = np.sum([np.prod(v.shape.as_list()) for v in tf.trainable_variables()])
+    log_string('parameter number: %f' % (parameter_num))
+
     class_accuracies = np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float)
     for i, name in enumerate(SHAPE_NAMES):
         log_string('%10s:\t%0.3f' % (name, class_accuracies[i]))
-    
 
 
 if __name__=='__main__':
